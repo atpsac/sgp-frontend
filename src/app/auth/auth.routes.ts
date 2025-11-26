26ab1d7de8e3b3b71f0d2bd502a2244a64a3cbdd
AlexLaime <alaimes64@gmail.com>
2025-11-26 18:15:06 -0500

    delete

--- src/app/auth/auth.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginPage } from './login.page/login.page';
-
-export const AUTH_ROUTES: Routes = [
-  { path: 'login', component: LoginPage, title: 'Login' }
-];
