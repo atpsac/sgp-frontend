87667f0c4e981d96d9e8c9436f33cdfcc57a6325
AlexLaime <alaimes64@gmail.com>
2025-11-26 23:03:16 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost/api-pharma/public/api/',
-  // assetsBaseUrl: 'http://localhost/api-pharma/public/uploads/productos/',
-
-    apiUrl: 'https://api-pharma.latamtic.com/api/',
-    assetsBaseUrl: 'https://api-pharma.latamtic.com/uploads/productos/',
-
-
+  apiUrl: 'http://161.132.194.105:3000', 
 };
